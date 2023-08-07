169363d03a6112e46fe7916253136116bd26d644
Allan Abaho <abahoallans@gmail.com>
2023-08-07 09:56:31 +0300

    php version changes

--- resources/views/common/sidebar.blade.php
+++ resources/views/common/sidebar.blade.php
@@ -28,6 +28,32 @@
 
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseZero" aria-expanded="true" aria-controls="collapseZero">
+            <i class="fa fa-qrcode"></i>
+            <span>Students</span>
+        </a>
+        <div id="collapseZero" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="{{route('codes.index')}}">List Students</a>
+                <a class="collapse-item" href="{{route('codes.create')}}">Add Student</a>
+            </div>
+        </div>
+    </li>
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+            <i class="fa fa-qrcode"></i>
+            <span>Staff Members</span>
+        </a>
+        <div id="collapseOne" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="{{route('codes.index')}}">List Staff</a>
+                <a class="collapse-item" href="{{route('codes.create')}}">Add Staff</a>
+            </div>
+        </div>
+    </li>
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
             <i class="fa fa-qrcode"></i>
             <span>QR Codes</span>
@@ -39,6 +65,26 @@
             </div>
         </div>
     </li>
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+            <i class="fa fa-qrcode"></i>
+            <span>Roles Management</span>
+        </a>
+        <div id="collapseThree" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="{{route('codes.index')}}">List Role</a>
+                <a class="collapse-item" href="{{route('codes.create')}}">Create Role</a>
+            </div>
+        </div>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" aria-expanded="true" aria-controls="collapseThree">
+            <i class="fa fa-cog-wheel"></i>
+            <span>Settings</span>
+        </a>
+    </li>
+
 
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">
